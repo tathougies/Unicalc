8e01b30537ca8a835d081b8143b105f5cafa88da
Travis Athougies <iammisc@gmail.com>
2010-09-10 18:07:32 -0700

    Added stub functions with documentation and documented all implemented functions

--- unicalc.rkt
+++ unicalc.rkt
@@ -29,8 +29,18 @@
 (define get-den third)
 
 ;; Add your unicalc functions below.
+; Function name: product
+; Input:
+;   L - a list of numbers
+; Output:
+;   The product of all the numbers in L
 (define (product L) (foldr multiply (make-QL 1 null null) L))
 
+; Function name: normalize-unit
+; Input:
+;   unit - a symbol referencing a defined unit
+; Output:
+;   A quantity list that expresses the unit in basic units
 (define (normalize-unit unit)
   (let ([next-unit (assoc unit unicalc-db)]) ;; Attempt to locate the unit in the unit database
     (if next-unit 
@@ -43,6 +53,27 @@
 	  (multiply (make-QL factor null null) (divide normalized-numerator normalized-denominator)))
 	(make-QL 1 (list unit) null)))) ;; Basic unit
 
+; Function name: normalize
+; Input:
+;   quantity - a quantity list
+; Output:
+;   A quantity list that has been normalized into the most basic units
+(define (normalize quantity)
+  (make-QL 0 '() '()))
+
+; Function name: cancel-unit
+; Input:
+;    num - a list of units in the numerator
+;    den - a list of units in the denominator
+;    ans - FOR INTERNAL USE ONLY. Always pass as null
+; Output:
+;    A list of the form (new-num new-den), where new-num and new-dem are
+;    the resulting numerator and denominator, respectively, after duplicate
+;    units have been cancelled out
+; Notes:
+;   In general, you never want to interact with this function, instead,
+;   call the cancel function which not only cancels out units but also
+;   normalizes the units, by sorting them alphabetically
 (define (cancel-unit num den ans)
   (cond
     [(null? num) (list ans den)]
@@ -54,6 +85,15 @@
 (test (cancel-unit '(second joule second) '(second second) '()) '((joule)()))
 (test (cancel-unit '(meters meters meters meters meters seconds meters seconds grams) '(meters meters meters seconds N grams grams) '()) '((seconds meters meters meters )(N grams)))
 
+; Function name: cancel
+; Input:
+;    num - a list of units in the numerator
+;    den - a list of units in the denominator
+; Output:
+;    A list of the form (new-num new-den), where new-num and new-dem are
+;    the resulting numerator and denominator, respectively, after duplicate
+;    units have been cancelled out. New-num and new-den are sorted to be
+;    in alphabetical order
 (define (cancel num den)
   (let [(l (cancel-unit num den '()))
         (symbol<? (lambda (x y) (string<? (string-downcase (symbol->string x)) (string-downcase (symbol->string y)))))]
@@ -62,6 +102,12 @@
 
 (test (cancel '(seconds grams meters kilograms N) '()) '((grams kilograms meters N seconds) ()))
 
+; Function name: multiply
+; Input:
+;   a - a normalized quantity list
+;   b - a normalized quantity list
+; Output:
+;   the product of a and b, as a normalized quantity list
 (define (multiply a b)
   (let* ([a-qt (get-quant a)]
 	 [a-num (get-num a)]
@@ -76,11 +122,49 @@
 	 [cancelled-den (second cancelled-numden)])
     (make-QL (* a-qt b-qt) cancelled-num cancelled-den)))
 
+; Function name: divide
+; Input:
+;  a - a normalized quantity list
+;  b - a normalized quantity list
+; Output:
+;  The quotient of a and b. This is identical to multiplying
+;  a by the reciprocal of b.
 (define (divide a b) ;; Multiply a by the reciprocal of b
   (let ([b-num (get-num b)]
 	[b-den (get-den b)]
 	[b-quant (get-quant b)])
     (multiply a (make-QL (/ 1 b-quant) b-den b-num))))
+
+; Function name: add
+; Input:
+;   a - a normalized quantity list
+;   b - a normalized quantity list
+; Output:
+;   The sum of a and b, so long as the units of a and b are
+;   interconvertible. If no suitable conversion can be found,
+;   an error is raised
+(define (add a b)
+  (make-QL 0 '() '()))
+
+; Function name: subtract
+; Input:
+;   a - a normalized quantity list
+;   b - a normalized quantity list
+; Output:
+;   The difference a - b. This is identical to the sum of
+;   a and the inverse of b. If the units of a and b are not
+;   interconvertible, this function will complain
+(define (subtract a b)
+  (add a (make-QL (- (get-quant b)) (get-num b) (get-den b))))
+
+; Function name: power
+; Input:
+;   a - a normalized quantity list
+;   p - an integer
+; Output:
+;   Returns a to the p'th power, with the proper units
+(define (power a p)
+  (make-QL 0 '() '()))
 
 ;; Load and run the tests
 (load "unicalc-tests.rkt")
